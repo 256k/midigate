9c36b241c094946ef328750144358208ab8f4d07
256k <fayek.helmi@gmail.com>
2023-02-23 20:40:28 -0500

    midigate wip init

---
+++ README.md
@@ -0,0 +1,1 @@
+# midigate
---
+++ midigate/midigate.ino
@@ -0,0 +1,68 @@
+
+/*
+ * midigate
+ *
+ * convert incoming usb midi notes into discrete gate output signals
+ *
+ *
+ */ 
+
+#include "MIDIUSB.h"
+
+// list of all root notes in order of midi note number (0-11)
+char *notes[] = {"c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"};
+
+
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+}
+
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
+
+void loop() {
+  midiEventPacket_t rx;
+  do {
+    rx = MidiUSB.read();
+    
+    if (rx.header != 0) {
+      Serial.println(notes[1]);
+      Serial.println(notes[(rx.byte2 % 12)]);
+      Serial.print("Received: ");
+      Serial.print(rx.header, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte1, HEX);
+      Serial.print("-");
+      // Serial.print(rx.byte2, HEX);
+      Serial.print(rx.byte2);
+      Serial.print("-");
+      Serial.println(rx.byte3, HEX);
+    }
+
+    
+  } while (rx.header != 0);
+}
